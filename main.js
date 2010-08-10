d54f67f4383a0ac25a24a4e2495bd821d20b5bbc
aimakun <aimakung@gmail.com>
2010-08-10 14:23:15 +0700

    * Add content title & imagefield description. * Move thumbnail box seperate from content body. * Fixed sometimes lightbox was loaded before updated contents are done. * Wrap search form for HTML validations.

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ var jQ = jQuery.noConflict();
 jQ(document).ready(function (e) {
   jQ.get('json?type=tree', function(response) {
     data = jQ.parseJSON(response);
-    jQ('#show').jstree({
+    jQ('#jstree_show').jstree({
       'json_data' : {
         'data' : data,
         'ajax' : {
@@ -12,12 +12,13 @@ jQ(document).ready(function (e) {
           }
         }
       },
+      'search' : { 'case_insensitive' : true },
       'plugins' : [ 'themes', 'json_data','search' ,'ui']
     });
   
   var current_item = '';
   
-  jQ('#show li a').each( function(index) {
+  jQ('#jstree_show li a').each( function(index) {
     var link = $(this).parent().attr('link');
     if (link !== undefined) {
       $(this).attr('href', link);
@@ -30,14 +31,20 @@ jQ(document).ready(function (e) {
           target = current_item = target[last_index];
           $.get(document.location.href + '/node/' + target, function(data) {
             $('#jstree_content').html(data);
+            $('#jstree_thumb').html($('#image-items-hidden').html());
+            $('#image-items-hidden').detach('');
+            if ($('#jstree_thumb').html().length > 0) {
+              Lightbox.initList();
+            }
           });
         }
       });
     }
   });
 
-    jQ('#btn').click(function(){
-       jQ('#show').jstree("search",jQ('#search_input').val());
+    jQ('#btn').click(function(e){
+       e.preventDefault();
+       jQ('#jstree_show').jstree("search",jQ('#search_input').val());
      }
 
    );
